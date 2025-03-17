eebb74ed00adf4d73edfd5437f2a71c19f019445
Robert-Kovesi-Profile <65221316+Robert-Kovesi-Profile@users.noreply.github.com>
2025-03-17 19:46:24 +0100

    personalize to Robert

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,7 +9,7 @@ export const auth = defineAuth({
   loginWith: {
     email: {
       verificationEmailStyle: "CODE",
-      verificationEmailSubject: "Welcome to the AI-Powered Recipe Generator!",
+      verificationEmailSubject: "Robert welcomes You to the AI-powered Recipe Generator!",
       verificationEmailBody: (createCode) =>
         `Use this code to confirm your account: ${createCode()}`,
     },
--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
     <div className="app-container">
       <div className="header-container">
         <h1 className="main-header">
-          Meet Your Personal
+          Robert welcomes You to Your Personal
           <br />
           <span className="highlight">Recipe AI</span>
         </h1>
